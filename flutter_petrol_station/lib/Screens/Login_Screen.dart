9eda57b38bdb9522b59e0201a02472b36a7b2470
Hussein B. Fawaz <hbfawaz112@gmail.com>
2021-09-17 20:21:31 +0300

    Update Login_Screen.dart

--- flutter_petrol_station/lib/Screens/Login_Screen.dart
+++ flutter_petrol_station/lib/Screens/Login_Screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_petrol_station/Screens/Dashboard.dart';
-
 import 'package:flutter_petrol_station/constants.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dashboard_firstore.dart';
 
 class Login extends StatefulWidget {
   static String id = 'login';
@@ -20,7 +20,7 @@ class _LoginState extends State<Login> {
   String email, password, emailE = ' ', passE = ' ', error = 'Enter some text';
   Color colorE = Colors.blueAccent, colorP = Colors.blueAccent;
   int passError = -1, emailError = -1, loggedIn = 0;
-
+  bool isloading=false;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -33,98 +33,118 @@ class _LoginState extends State<Login> {
                 Expanded(
                   child: Container(
                     child: Image(
-                      image: AssetImage("assets/pic.png"),
-                      width: 280,height: 280,
+                      image: AssetImage("images/pp.jpg"),
                     ),
                   ),
                 ),
                 Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      children: <Widget>[
-                        TextField(
-                          keyboardType: TextInputType.emailAddress,
-                          textAlign: TextAlign.center,
-                          decoration: kTextFieldDecoration.copyWith(
-                            hintText: 'Enter your Email',
-                            suffixIcon: Icon(Icons.email),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: colorE),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(32.0)),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: colorE, width: 2.0),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(32.0)),
+                  child: Column(
+                    children: <Widget>[
+                      isloading==true?CircularProgressIndicator():Text(''),
+                      TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        textAlign: TextAlign.center,
+                        decoration: kTextFieldDecoration.copyWith(
+                          hintText: 'Enter your Email',
+                          suffixIcon: Icon(Icons.email),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: colorE),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(32.0)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: colorE, width: 2.0),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(32.0)),
+                          ),
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            colorE = Colors.blueAccent;
+                          });
+                          email = value;
+                        },
+                      ),
+                      SizedBox(
+                        height: 1.0,
+                      ),
+                      Text(
+                        emailError == 1 ? 'Enter some text' : '',
+                        style: TextStyle(color: colorE),
+                      ),
+                      TextField(
+                        obscureText: true,
+                        textAlign: TextAlign.center,
+                        decoration: kTextFieldDecoration.copyWith(
+                          hintText: 'Enter your password',
+                          suffixIcon: Icon(Icons.remove_red_eye),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: colorP),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(32.0)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: colorP, width: 2.0),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(32.0)),
+                          ),
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            colorP = Colors.blueAccent;
+                          });
+                          password = value;
+                        },
+                      ),
+                      SizedBox(
+                        height: 1.0,
+                      ),
+                      Text(
+                        passError == 1 ? 'Enter some text' : '',
+                        style: TextStyle(color: colorP),
+                      ),
+                      TextButton(
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.blueAccent),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
                             ),
                           ),
-                          onChanged: (value) {
+                        ),
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        onPressed: () async {
+                          if (email == null) {
+                            print('email empty');
                             setState(() {
-                              colorE = Colors.blueAccent;
+                              emailError = 1;
+                              colorE = Colors.red;
+                              emailE = 'Enter some text';
+                              print('color Email:');
+                              print(colorE.value);
+                              print(emailError);
                             });
-                            email = value;
-                          },
-                        ),
-                        SizedBox(
-                          height: 1.0,
-                        ),
-                        Text(
-                          emailError == 1 ? 'Enter some text' : '',
-                          style: TextStyle(color: colorE),
-                        ),
-                        TextField(
-                          obscureText: true,
-                          textAlign: TextAlign.center,
-                          decoration: kTextFieldDecoration.copyWith(
-                            hintText: 'Enter your password',
-                            suffixIcon: Icon(Icons.remove_red_eye),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: colorP),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(32.0)),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: colorP, width: 2.0),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(32.0)),
-                            ),
-                          ),
-                          onChanged: (value) {
+                          }
+                          if (password == null) {
+                            print('password empty');
                             setState(() {
-                              colorP = Colors.blueAccent;
+                              passError = 1;
+                              colorP = Colors.red;
+                              passE = 'Enter some text';
+                              print('color password:');
+                              print(colorP.value);
+                              print(passError);
                             });
-                            password = value;
-                          },
-                        ),
-                        SizedBox(
-                          height: 1.0,
-                        ),
-                        Text(
-                          passError == 1 ? 'Enter some text' : '',
-                          style: TextStyle(color: colorP),
-                        ),
-                        TextButton(
-                          style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all(Colors.blueAccent),
-                            shape:
-                                MaterialStateProperty.all<RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                              ),
-                            ),
-                          ),
-                          child: Text(
-                            'Login',
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                          onPressed: () async {
-                            if (email == null) {
-                              print('email empty');
+                          }
+                          if (email != null && password != null) {
+                            if (email.isEmpty) {
                               setState(() {
                                 emailError = 1;
                                 colorE = Colors.red;
@@ -134,8 +154,7 @@ class _LoginState extends State<Login> {
                                 print(emailError);
                               });
                             }
-                            if (password == null) {
-                              print('password empty');
+                            if (password.isEmpty) {
                               setState(() {
                                 passError = 1;
                                 colorP = Colors.red;
@@ -145,51 +164,47 @@ class _LoginState extends State<Login> {
                                 print(passError);
                               });
                             }
-                            if (email != null && password != null) {
-                              if (email.isEmpty) {
-                                setState(() {
-                                  emailError = 1;
-                                  colorE = Colors.red;
-                                  emailE = 'Enter some text';
-                                  print('color Email:');
-                                  print(colorE.value);
-                                  print(emailError);
-                                });
-                              }
-                              if (password.isEmpty) {
-                                setState(() {
-                                  passError = 1;
-                                  colorP = Colors.red;
-                                  passE = 'Enter some text';
-                                  print('color password:');
-                                  print(colorP.value);
-                                  print(passError);
-                                });
-                              }
-                              try {
-                                final user =
-                                    await _auth.signInWithEmailAndPassword(
-                                        email: email, password: password);
+                            try {
+                              final user =
+                                  await _auth.signInWithEmailAndPassword(
+                                      email: email, password: password);
 
-                                if (user != null) {
-                                  //Navigator.pushNamed(context, Containers.id);
-                                Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Dashboard()));
-                                  setState(() {
-                                    emailError = 0;
-                                    passError = 0;
-                                    colorE = Colors.blueAccent;
-                                    colorP = Colors.blueAccent;
-                                  });
-                                }
-                              } catch (e) {
-                                print(e);
+                              if (user != null) {
+                                setState(() {
+                                                                  isloading=true;
+                                                                });
+                                //Navigator.pushNamed(context, Containers.id);
+                                SharedPreferences preferences = await SharedPreferences.getInstance();
+                                preferences.setString('login', 'ok');
+                                print("The shared preference has been full , ${preferences.get('login')}");
+                               Navigator
+    .of(context)
+    .pushReplacement(new MaterialPageRoute(builder: (BuildContext context) => Dashboard()));
+      
+                                setState(() {
+                                  emailError = 0;
+                                  passError = 0;
+                                  colorE = Colors.blueAccent;
+                                  colorP = Colors.blueAccent;
+                                });
                               }
+                              
+                            } catch (e) {
+                              print("**********here ${e}**************");
+                              Fluttertoast.showToast(
+        msg: "User not exist, try again..",
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 22.0
+    );
                             }
-                          },
-                        ),
-                      ],
-                    ),
+                          }
+                        },
+                      ),
+                    ],
                   ),
                 ),
               ],
