e14faaa7d1d98a5095c9560c3d15781e61ee8e5f
Hussein B. Fawaz <hbfawaz112@gmail.com>
2021-09-17 20:20:31 +0300

    Update Dashboard.dart

--- flutter_petrol_station/lib/Screens/Dashboard.dart
+++ flutter_petrol_station/lib/Screens/Dashboard.dart
@@ -1,14 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:double_back_to_close_app/double_back_to_close_app.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-//import 'package:flutter_petrol_station/Services/authProvider.dart';
 import 'package:flutter_petrol_station/Services/cloud_services.dart';
 import 'package:flutter_petrol_station/Widgets/Drawer.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
-import 'package:flutter_petrol_station/Screens/Pump_Records.dart';
-import 'Login_Screen.dart';
-import 'Pump_Records.dart';
-import 'Shipments.dart';
+import 'package:flutter_petrol_station/Screens/pump_records.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+//import 'pump_records.dart';
+
+import 'shipments.dart';
+import 'package:flutter_petrol_station/Screens/login.dart';
+
 class Dashboard extends StatefulWidget {
   static String id = "Dashboard";
   @override
@@ -71,8 +74,11 @@ class _DashboardState extends State<Dashboard> {
         actions: [
           InkWell(
               onTap: () async {
-               // await AuthProvider().logOut();
-                Navigator.push(
+               
+                 SharedPreferences preferences = await SharedPreferences.getInstance();
+              preferences.clear();
+               FirebaseAuth.instance.signOut();
+                Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(builder: (context) => Login()),
                 );
@@ -92,153 +98,158 @@ class _DashboardState extends State<Dashboard> {
         ],
       ),
       drawer: getDrawer(),
-      body: StreamBuilder(
-          stream: Stream.fromFuture(getContainers()),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: snapshot.data.docs.length,
-                  itemBuilder: (context, index) {
-                    DocumentSnapshot documentSnapshot =
-                        snapshot.data.docs[index];
-                    return Padding(
-                      padding: const EdgeInsets.all(18.0),
-                      child: Card(
-                        elevation: 12.0,
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              Container(
-                                color: Colors.lightBlue,
-                                width: MediaQuery.of(context).size.width,
-                                height: MediaQuery.of(context).size.height /
-                                        snapshot.data.docs.length +
-                                    50,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(15.0),
-                                  child: Column(children: [
-                                    Text(
-                                        'Container ${documentSnapshot["Container_Name"]}',
-                                        style: TextStyle(
-                                            color: Colors.indigo[900],
-                                            fontSize: 37,
-                                            fontWeight: FontWeight.w900)),
-                                    SizedBox(height: 20),
-                                    Expanded(
-                                        child: LinearPercentIndicator(
-                                      width: MediaQuery.of(context).size.width -
-                                          MediaQuery.of(context).size.width / 5,
-                                      animation: true,
-                                      lineHeight: 20.0,
-                                      animationDuration: 2500,
-                                      percent: documentSnapshot["Volume"] /
-                                          documentSnapshot["Capacity"],
-                                      center: Text(
-                                        '${documentSnapshot["Volume"]} L',
-                                        style: TextStyle(fontSize: 20.0),
-                                      ),
-                                      linearStrokeCap: LinearStrokeCap.roundAll,
-                                      progressColor: Colors.green,
-                                    )),
-                                  ]),
-                                ),
-                              ),
-                              SizedBox(
-                                height: 20,
-                              ),
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    "Pumps  ",
-                                    style: TextStyle(
-                                        fontSize: 22,
-                                        color: Colors.indigo[200],
-                                        fontWeight: FontWeight.w800),
+      body: DoubleBackToCloseApp(
+        snackBar: const SnackBar(
+            content: Text('Tap back again to leave'),
+          ),
+        child: StreamBuilder(
+            stream: Stream.fromFuture(getContainers()),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: snapshot.data.docs.length,
+                    itemBuilder: (context, index) {
+                      DocumentSnapshot documentSnapshot =
+                          snapshot.data.docs[index];
+                      return Padding(
+                        padding: const EdgeInsets.all(18.0),
+                        child: Card(
+                          elevation: 12.0,
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Container(
+                                  color: Colors.lightBlue,
+                                  width: MediaQuery.of(context).size.width,
+                                  height: MediaQuery.of(context).size.height /
+                                          snapshot.data.docs.length +
+                                      50,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(15.0),
+                                    child: Column(children: [
+                                      Text(
+                                          'Container ${documentSnapshot["Container_Name"]}',
+                                          style: TextStyle(
+                                              color: Colors.indigo[900],
+                                              fontSize: 37,
+                                              fontWeight: FontWeight.w900)),
+                                      SizedBox(height: 20),
+                                      Expanded(
+                                          child: LinearPercentIndicator(
+                                        width: MediaQuery.of(context).size.width -
+                                            MediaQuery.of(context).size.width / 5,
+                                        animation: true,
+                                        lineHeight: 20.0,
+                                        animationDuration: 2500,
+                                        percent: documentSnapshot["Volume"] /
+                                            documentSnapshot["Capacity"],
+                                        center: Text(
+                                          '${documentSnapshot["Volume"]} L',
+                                          style: TextStyle(fontSize: 20.0),
+                                        ),
+                                        linearStrokeCap: LinearStrokeCap.roundAll,
+                                        progressColor: Colors.green,
+                                      )),
+                                    ]),
                                   ),
-                                  Center(
-                                      child: StreamBuilder(
-                                          stream: Stream.fromFuture(
-                                              getPumpNames(documentSnapshot[
-                                                  "Container_Id"])),
-                                          builder: (context, snapshot) {
-                                            if (snapshot.hasData) {
-                                              return ListView.builder(
-                                                  shrinkWrap: true,
-                                                  itemCount:
-                                                      snapshot.data.docs.length,
-                                                  itemBuilder:
-                                                      (context, index) {
-                                                    DocumentSnapshot
-                                                        documentSnapshot1 =
-                                                        snapshot
-                                                            .data.docs[index];
-                                                    return Padding(
-                                                      padding:
-                                                          const EdgeInsets.all(
-                                                              8.0),
-                                                      child: Center(
-                                                        child: InkWell(
-                                                          onTap: () {
-                                                            Navigator.push(
-                                                              context,
-                                                              MaterialPageRoute(
-                                                                  builder: (context) => Pump_Records(
-                                                                      pumpID: documentSnapshot1[
-                                                                          "Pump_Id"],
-                                                                      pumpName:
-                                                                          documentSnapshot1[
-                                                                              "Pump_Name"])),
-                                                            );
-                                                          },
-                                                          child: Text(
-                                                            '    ${documentSnapshot1["Pump_Name"]}',
-                                                            style: TextStyle(
-                                                                fontSize: 20,
-                                                                color: Colors
-                                                                        .indigo[
-                                                                    200]),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      "Pumps  ",
+                                      style: TextStyle(
+                                          fontSize: 22,
+                                          color: Colors.indigo[200],
+                                          fontWeight: FontWeight.w800),
+                                    ),
+                                    Center(
+                                        child: StreamBuilder(
+                                            stream: Stream.fromFuture(
+                                                getPumpNames(documentSnapshot[
+                                                    "Container_Id"])),
+                                            builder: (context, snapshot) {
+                                              if (snapshot.hasData) {
+                                                return ListView.builder(
+                                                    shrinkWrap: true,
+                                                    itemCount:
+                                                        snapshot.data.docs.length,
+                                                    itemBuilder:
+                                                        (context, index) {
+                                                      DocumentSnapshot
+                                                          documentSnapshot1 =
+                                                          snapshot
+                                                              .data.docs[index];
+                                                      return Padding(
+                                                        padding:
+                                                            const EdgeInsets.all(
+                                                                8.0),
+                                                        child: Center(
+                                                          child: InkWell(
+                                                            onTap: () {
+                                                              Navigator.push(
+                                                                context,
+                                                                MaterialPageRoute(
+                                                                    builder: (context) => Pump_Records(
+                                                                        pumpID: documentSnapshot1[
+                                                                            "Pump_Id"],
+                                                                        pumpName:
+                                                                            documentSnapshot1[
+                                                                                "Pump_Name"])),
+                                                              );
+                                                            },
+                                                            child: Text(
+                                                              '    ${documentSnapshot1["Pump_Name"]}',
+                                                              style: TextStyle(
+                                                                  fontSize: 20,
+                                                                  color: Colors
+                                                                          .indigo[
+                                                                      200]),
+                                                            ),
                                                           ),
                                                         ),
-                                                      ),
-                                                    );
-                                                  });
-                                            } else {
-                                              return Text('Loaddinggg....');
-                                            }
-                                          })),
-                                ],
-                              ),
-                              SizedBox(height: 5.0),
-                              ButtonTheme(
-                                height: 50.0,
-                                minWidth: 270,
-                                child: RaisedButton(
-                                  color: Colors.grey,
-                                  elevation: 12,
-                                  child: Text('New Shipment',
-                                      style: TextStyle(
-                                          color: Colors.white, fontSize: 23)),
-                                  onPressed: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => Shipments()),
-                                    );
-                                  },
+                                                      );
+                                                    });
+                                              } else {
+                                                return Text('Loaddinggg....');
+                                              }
+                                            })),
+                                  ],
                                 ),
-                              ),
-                              SizedBox(height: 25.0),
-                            ]),
-                      ),
-                    );
-                  });
-            } else {
-              return Center(child: CircularProgressIndicator());
-            }
-          }),
+                                SizedBox(height: 5.0),
+                                ButtonTheme(
+                                  height: 50.0,
+                                  minWidth: 270,
+                                  child: RaisedButton(
+                                    color: Colors.grey,
+                                    elevation: 12,
+                                    child: Text('New Shipment',
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 23)),
+                                    onPressed: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => Shipments()),
+                                      );
+                                    },
+                                  ),
+                                ),
+                                SizedBox(height: 25.0),
+                              ]),
+                        ),
+                      );
+                    });
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            }),
+      ),
     );
   }
 }
